0a145b338eebbfa598bc654c36158bed6e5f970c
kou3Akina <41255005+kou3Akina@users.noreply.github.com>
2018-07-17 20:57:44 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+new Vue({
+    el: '#app',
+    data: {
+    newtodo: "",
+    todos: []
+    },
+    methods: {
+      addTodo: function(event) {
+        event.preventDefault();
+        if (this.newtodo == "") return;
+        this.todos.push({
+          item: this.newtodo
+        });
+        this.newtodo = "";
+      },
+      deleteTodo: function(todo) {
+        var index = this.todos.indexOf(todo);
+        this.todos.splice(index, 1)
+      }
+    }
+})
